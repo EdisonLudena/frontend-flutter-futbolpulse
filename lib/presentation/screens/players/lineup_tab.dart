7518ab8024a0e32764a394d51a17178786b62a9d
SA-1078 <santiagocolimba10@gmail.com>
2026-07-13 22:40:30 -0500

    feat(ui): actualiza tab de alineacion en la gestion de plantilla

--- lib/presentation/screens/players/lineup_tab.dart
+++ lib/presentation/screens/players/lineup_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../../theme/app_colors.dart';
 import '../../../theme/app_text_styles.dart';
 import '../../providers/repository_providers.dart';

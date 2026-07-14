de6e37c4043e9090b4eeab29d386e9f6ba72d551
SA-1078 <santiagocolimba10@gmail.com>
2026-07-13 22:40:11 -0500

    feat(ui): actualiza pantalla de detalle y ficha individual del jugador

--- lib/presentation/screens/players/player_detail_screen.dart
+++ lib/presentation/screens/players/player_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
 import '../../../domain/model/jugador.dart';
 import '../../../domain/model/valoracion_economica.dart';
 import '../../../theme/app_colors.dart';

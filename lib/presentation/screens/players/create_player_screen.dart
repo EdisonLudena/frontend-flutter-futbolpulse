24f229a0d76034fbbf039f761412a155a7978955
SA-1078 <santiagocolimba10@gmail.com>
2026-07-13 22:39:56 -0500

    feat(ui): mejora formulario de registro de nuevo jugador

--- lib/presentation/screens/players/create_player_screen.dart
+++ lib/presentation/screens/players/create_player_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
 import '../../providers/repository_providers.dart';
 import '../../providers/ui_provider.dart';
 import 'players_screen.dart';

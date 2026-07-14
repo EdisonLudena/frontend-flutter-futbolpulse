931f5caa9d5953c79759078c9636d9262b664e7e
SA-1078 <santiagocolimba10@gmail.com>
2026-07-13 22:39:43 -0500

    feat(ui): actualiza vista principal de gestion de jugadores

--- lib/presentation/screens/players/players_screen.dart
+++ lib/presentation/screens/players/players_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+
 import '../../providers/repository_providers.dart';
 import '../../../domain/model/jugador.dart';
 import '../../../theme/app_colors.dart';
